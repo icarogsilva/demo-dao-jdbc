b0caaaa1dbee25e94b356765d24beabe77c93409
icarogsilva <icaro_g_silva@hotmail.com>
2020-08-19 15:08:57 -0300

    update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,21 @@ public class Program {
 		Seller seller = sellerDao.findById(3);		
 		System.out.println(seller);
 		
-		System.out.println("\n===Test 2: seller findByDepartment ===");
+		System.out.println("\n===Test 4: seller insert ===");
+		Department department = new Department(2, null);
+		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = "+ newSeller.getId());
+
+		
+		System.out.println("\n===Test 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
+		
+		/*System.out.println("\n===Test 2: seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
@@ -35,7 +49,7 @@ public class Program {
 		System.out.println("\n===Test 4: seller insert ===");
 		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = "+ newSeller.getId());
+		System.out.println("Inserted! New id = "+ newSeller.getId());*/
 
 	}
 
